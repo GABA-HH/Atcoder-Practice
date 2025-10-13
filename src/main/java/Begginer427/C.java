999b4ecda47b66e72ca4c0349fe1515c0050527f
GABA-HH <kamomekaraage@gmail.com>
2025-10-13 17:12:13 +0900

    Contest427 problem:C AC

--- src/main/java/Begginer427/C.java
+++ src/main/java/Begginer427/C.java
@@ -1,35 +1,63 @@
 package Begginer427;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
+// 実装方針：
+// 辺削除の全探索を先に行ってしまうと、TLEになってしまうので、先に頂点の色分けをした後に、同じ色の頂点をつないでいる辺削除を行うことで計算量を削減する。
+// グローバル変数に辺の最小削除回数をもっておき、色の組み合わせ事の辺の削除回数と比較していくことで、最小の削除回数を求める
+
+
 public class C {
+    private static int minDeleteNum = 0;
+
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+
+            // 頂点と辺の数を取得。配列の番号に合わせて-1をする            
             int n = sc.nextInt();
             int m = sc.nextInt();
 
-            // 各頂点の色を保持する配列　黒＝0、白＝1
-            int[] node = new int[n];
+            int[] node = new int[n];  /* 各頂点の色を保持する配列　黒＝0、白＝1 */
+            int[][] edge = new int[m][2]; /* 各辺がつなぐ頂点を保持する配列 */
 
-            int[][] edge = new int[m][2];
-
+            // 辺の情報を取得
             for (int i=0; i<m; i++) {
-                for (int j=0; j<2; j++) {
-                    edge[i][j] = sc.nextInt();
-                }
+                edge[i][0] = sc.nextInt()-1;
+                edge[i][1] = sc.nextInt()-1;
             }
             
-            for (int i=0; i<m; i++) {
-                for (int j=0; j<2; j++) {
-                    for
-                    
+            minDeleteNum = edge.length;
+            
+            C c = new C();
+            c.setColor(0, n, node, edge);
+
+            System.out.println(minDeleteNum);
+        } 
+    }
+
+    // 再起呼び出しで各頂点の色の組み合わせを全探索
+    private void setColor(int v,int n,int[] node,int[][] edge) {
+        
+        if (v==n) {
+            // System.out.println(Arrays.toString(node));
+            int DeleteNum = 0;
+
+            for(int i=0; i<edge.length; i++) {
+                if (node[edge[i][0]]==node[edge[i][1]]) {
+                    DeleteNum++;
                 }
             }
-            // for () {
 
-            // }
+            minDeleteNum = Math.min(minDeleteNum, DeleteNum);
+            return;
+        }
 
-        } 
+        // 白に設定
+        node[v]=1;
+        setColor(v+1,n, node, edge);
+        
+        // 黒に設定
+        node[v]=0;
+        setColor(v+1,n, node, edge);
     }
 }
