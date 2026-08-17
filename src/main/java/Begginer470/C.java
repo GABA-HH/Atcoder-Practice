782525127098ae40e81eac039f9ef656fc320200
GABA-HH <kamomekaraage@gmail.com>
2026-08-17 22:22:30 +0900

    contest470 C問題 WA

--- src/main/java/Begginer470/C.java
+++ src/main/java/Begginer470/C.java
@@ -1,11 +1,46 @@
 ﻿package Begginer470;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class C {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            // TODO: Solve Problem C
+            int N = sc.nextInt();
+            int Q = sc.nextInt();
+
+            int[] A = new int[N];
+
+            int queryType = 0;
+            int query1 = 1;
+            int query2 = 2;
+
+            for (int i = 0; i < Q; i++) {
+                queryType = sc.nextInt();
+
+                if (query1 == queryType) {
+                    query1(A, sc.nextInt() - 1);
+                } else if (query2 == queryType) {
+                    query2(A, i - 1);
+                }
+            }
+
+            System.out.println(Arrays.toString(A));
         }
+    }
+
+    private static int[] query1(int[] array, int x) {
+
+        array[x]++;
+        return array;
+    }
+
+    private static int[] query2(int[] array, int i) {
+
+        if (1 <= array[i]) {
+            array[i]--;
+        }
+
+        return array;
     }
 }
